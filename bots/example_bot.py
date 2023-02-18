b696196d59dd3af631dc51e4efcbcfb0d9117f36
Vishal Khopkar <vishalkhopkar20@gmail.com>
2023-02-18 00:38:47 -0500

    fixing bugs

--- bots/example_bot.py
+++ bots/example_bot.py
@@ -5,20 +5,21 @@ from src.map import TileInfo, RobotInfo
 import random
 import numpy as np
 
-
 class BotPlayer(Player):
     """
     Players will write a child class that implements (notably the play_turn method)
     """
 
     def getValue(self, x, y):
-        if x >= 0 and x < self.explored.shape[0] and y >= 0 and y < self.explored.shape[1]:
+        if 0 <= x < self.explored.shape[0] and 0 <= y < self.explored.shape[1]:
             return self.explored[x][y]
-        return False
+        return True
 
     def explore(self, rob, rname, game_state):
         # get the current location
-        x = rob.row, y = rob.col
+
+        x = rob.row
+        y = rob.col
         """
         explored[x][y + 1], explored[x][y - 1],
         explored[x + 1][y], explored[x - 1][y],
@@ -83,12 +84,8 @@ class BotPlayer(Player):
         elif not self.getValue(x - 2, y + 2):
             self.set_explored_true(x - 1, y + 1)
             game_state.move_robot(rname, Direction.UP_RIGHT)
-
-
-        pass
-
     def set_explored_true(self, x, y):
-        self.explored[x][y] = True
+
         for i in range(-1, 2):
             for j in range(-1, 2):
                 x1 = x + i
@@ -110,7 +107,7 @@ class BotPlayer(Player):
         # get turn/team info
         height, width = len(ginfo.map), len(ginfo.map[0])
         if self.explored is None:
-            self.explored = np.array(dtype=bool, shape=(height, width))
+            self.explored = np.ndarray(dtype=bool, shape=(height, width))
 
         # print info about the game
         print(f"Turn {ginfo.turn}, team {ginfo.team}")
@@ -149,7 +146,7 @@ class BotPlayer(Player):
         # iterate through dictionary of robots
         for rname, rob in robots.items():
             print(f"Robot {rname} at {rob.row, rob.col}")
-            if (rob.type == RobotType.EXPLORER):
+            if rob.type == RobotType.EXPLORER:
                 self.explore(rob, rname, game_state)
                 return
             # randomly move if possible
